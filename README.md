35ecb99bf2c890c7627c723510a27c6de9f59c73
Thukten-Norbu999 <thuktennorbu33@gmail.com>
2023-07-20 09:22:04 +0600

    project structure made

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# DataModels" 
+<h1>DATA MODELS</h1>
+
+<p>Trying to build a models module with machine learning features</p>
+<p>Might already be there or impractical but doing this to learn shit</p>
---
+++ copySQL/model.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+
+
+
+class Model:
+    def __init__(self):
+        pass
+
+
+    def is_valid(self):
+        pass
+
+
+    def save(self):
+        pass
+
+
+class Column(Model):
+    
+    def __init__(self):
+        pass
+
+
